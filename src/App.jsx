81d039737a7801629db27a0c3642749800334a2d
mohiuddin007 <mohiuddinmazumder94@gmail.com>
2024-01-04 18:08:47 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-slate-950	">
+      <h1 className="text-3xl font-thin text-red-500	">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
